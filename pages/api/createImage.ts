4f6a3f3b14e6635b1b4054fbfffbb04c46a7dee7
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-02-15 18:49:07 +0100

    improve folder structure for client images

--- pages/api/createImage.ts
+++ pages/api/createImage.ts
@@ -6,18 +6,29 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
         image: string,
         count: number,
         total: number
+        pass: string
     }
 
-    const pass = "test"
+    const { image, count, total, pass }: ReqBodyType = req.body;
 
-    const { image, count, total }: ReqBodyType = req.body;
-    const name = `./clientImages/${pass}${count}.jpeg`;
+    if (!image || !count || !total || !pass) {
+        return res.status(401)
+    }
+
+    const name = `./clientImages/${pass}/${count}.jpeg`;
     const base64Data = image.replace("data:image/jpeg;base64,", "");
 
+    // ROOT
     fs.mkdir('./clientImages', { recursive: true }, (err) => {
         err && console.log(err)
     });
 
+    // ONE CLIENT'S FOLDER
+    fs.mkdir(`./clientImages/${pass}`, { recursive: true }, (err) => {
+        err && console.log(err)
+    });
+
+    // CREATE SINGLE IMAGE .jpeg 
     fs.writeFile(name, base64Data, "base64", (err) => {
         err && console.log(err)
     })
